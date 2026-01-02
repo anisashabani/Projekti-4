2d5a95dbcdc3bec67181bacb2cbb3d04456a0577
rinatroni-lab <rina.troni@student.uni-pr.edu>
2026-01-02 01:48:46 +0100

    implementimi i shtyp() dhe lexo()

--- Projekti 4/Projekti 4.cpp
+++ Projekti 4/Projekti 4.cpp
@@ -26,6 +26,38 @@ struct Student {
     void shtyp() const;
 };
 
+void Student::lexo() {
+    cout << "ID: ";
+    cin >> ID;
+    cout << "Emri: ";
+    cin >> Emri;
+    cout << "Mbiemri: ";
+    cin >> Mbiemri;
+
+    cout << "\nShkruaj notat (0-10). Notat < 5 NUK llogariten ne mesatare.\n";
+    for (int i = 0; i < NR_LENDEVE; i++) {
+        cout << "Nota - " << emriLendes[i] << ": ";
+        cin >> Nota[i];
+    }
+}
+
+void Student::shtyp() const {
+    cout << "\n--- STUDENT ---\n";
+    cout << "ID: " << ID << endl;
+    cout << "Emri: " << Emri << endl;
+    cout << "Mbiemri: " << Mbiemri << endl;
+
+    cout << "Notat:\n";
+    for (int i = 0; i < NR_LENDEVE; i++) {
+        cout << "  - " << emriLendes[i] << ": " << Nota[i] << endl;
+    }
+
+    cout << "Mesatarja (pa notat < 5): " << mesatare() << endl;
+}
+if (n == 0) {
+    cout << "\nNuk u shtua asnje student.\n";
+    return 0;
+}
 
 int main()
 {
