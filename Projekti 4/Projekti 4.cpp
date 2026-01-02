4063af11aeb8aff8ee78019be4ede5a331fd238f
orgesaqamili <orgesa.qamili@student.uni-pr.edu>
2026-01-02 14:52:20 +0100

    Implement student average (ignore grades < 5)

--- Projekti 4/Projekti 4.cpp
+++ Projekti 4/Projekti 4.cpp
@@ -25,6 +25,20 @@ struct Student {
     double mesatare() const;
     void shtyp() const;
 };
+double Student::mesatare() const {
+    double s = 0;
+    int count = 0;
+
+    for (int i = 0; i < NR_LENDEVE; i++) {
+        if (Nota[i] >= 5) {
+            s += Nota[i];
+            count++;
+        }
+    }
+
+    if (count == 0) return 0;
+    return s / count;
+}
 
 
 int main()
